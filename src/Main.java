5fbe8b2f619101657aa438f66f1582e99d3f283a
Christilyn A <christilynarjona@outlook.com>
2018-03-12 15:16:15 -0600

    references count

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,12 @@ public class Main {
 
     private static int declarationsCount = 0;
     private static int referencesCount = 0;
-    private static String javaType = "String";
+    private static String javaType;
 
-    //use ASTParse to parse string
+    /**
+     * Parse the string (file content) and count declarations and references
+     * @param str string to parse
+     */
     private static void parse(String str) {
         ASTParser parser = ASTParser.newParser(AST.JLS3);
         parser.setSource(str.toCharArray());
@@ -34,8 +37,8 @@ public class Main {
                 if(node.getParent() instanceof FieldDeclaration){
                     FieldDeclaration declaration = ((FieldDeclaration) node.getParent());
                     if(declaration.getType().isSimpleType()){
-                        typeSimpleName = declaration.getType().toString();
-                        if (typeSimpleName.equals(javaType)) {
+                        typeSimpleName = declaration.getType().toString().toLowerCase();
+                        if (typeSimpleName.contains(javaType.toLowerCase())) {
                             declarationsCount++;
                             this.names.add(name.getIdentifier());
                         }
@@ -57,7 +60,12 @@ public class Main {
 
     }
 
-    //read file content into a string
+    /**
+     * Reads content of a file into a string
+     * @param filePath absolute path of file
+     * @return contents of file
+     * @throws IOException e
+     */
     private static String readFileToString(String filePath) throws IOException {
         StringBuilder fileData = new StringBuilder(1000);
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
@@ -75,14 +83,19 @@ public class Main {
         return  fileData.toString();
     }
 
-    //loop directory to get file list
-    private static void ParseFilesInDir() throws IOException{
+    /**
+     * Read all files in specified directory
+     * @throws IOException e
+     */
+    private static void readFilesInDir() throws IOException{
 //        Scanner scan = new Scanner(System.in);
 //        String pathname = scan.nextLine();
 //        String javaType = scan.nextLine();
         String pathname = "C:\\Users\\stealth 2017\\eclipse-workspace\\SENG3";
+        javaType = "String";
         File dirs = new File(pathname);
         String dirPath = dirs.getCanonicalPath() + File.separator+"src"+File.separator;
+        
 
         File root = new File(dirPath);
         File[] files = root.listFiles ( );
@@ -98,7 +111,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        ParseFilesInDir();
+        readFilesInDir();
         System.out.print(javaType + ". Declarations found: " + declarationsCount + "; ");
         System.out.println("references found: " + referencesCount);
     }
