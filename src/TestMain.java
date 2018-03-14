106407b86d789b50c19f5f5f42537800907085b1
3ternalDeath <abmisawesome@gmail.com>
2018-03-13 19:12:04 -0600

    making tests, will continue at home

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,11 +1,11 @@
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
 
-import org.junit.After;
 import org.junit.Test;
 
 public class TestMain{
@@ -14,8 +14,9 @@ public class TestMain{
 	
 	@Test
 	public void testFileFinding() {
+		String path = BASEDIR + "\\testFiles1";
 		try {
-			File[] files = Main.readFilesInDir(BASEDIR + "\\testFiles1");
+			File[] files = Main.readFilesInDir(path);
 			assertEquals(files.length, 2);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -23,36 +24,82 @@ public class TestMain{
 		}
 	}
 	
-	
-	
-	@After
-	public void teardown() {
-		System.out.println();
-		System.out.println();
+	@Test
+	public void testFileFinding2() {
+		String path = BASEDIR + "\\testFiles1";
+		try {
+			File[] files = Main.readFilesInDir(path);
+			assertEquals(files[0], new File(path + "\\bye.java"));
+			assertEquals(files[1], new File(path + "\\hi.java"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
 	
-	
-	
-	@Test
-	public void testUnQualifiedName() {
-		Main.main(new String[] {BASEDIR + "\\testFiles1", "String"});
-	}
-	
-	
-	
-	@Test
-	public void testQualifiedName() {
-		Main.main(new String[] {BASEDIR + "\\testFiles1", "java.lang.String"});
-	}
-	
-	@Test
-	public void testUnQualifiedName2() {
-		Main.main(new String[] {BASEDIR + "\\testFiles1", "hi"});
+	@Test(expected = ArrayIndexOutOfBoundsException.class)
+	public void testFileFinding3() {
+		String path = BASEDIR + "\\testFileEmp";
+		
+		try {
+			File[] files = Main.readFilesInDir(path);
+			files[0] = null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+		
 	}
 	
 	@Test
-	public void testUnQualifiedName3() {
-		Main.main(new String[] {BASEDIR + "\\testFiles1", "Go"});
+	public void testReadFile() {
+		String path = BASEDIR + "\\testRead\\uReadBro.txt";
+		String test = "\r\n" + 
+				"hi\r\n" + 
+				"\r\n" + 
+				"k";
+		try {
+			String src = Main.readFileToString(path);
+			assertEquals(src, test);
+		} catch (IOException e) {
+			System.err.println("Bad Test Setup");
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testReadFile2() {
+		String path = BASEDIR + "\\testRead\\nofingHere.txt";
+		String test = "";
+		try {
+			String src = Main.readFileToString(path);
+			assertEquals(src, test);
+		} catch (IOException e) {
+			System.err.println("Bad Test Setup");
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testReadFile3() {
+		String path = BASEDIR + "\\testRead\\lastOneIPromise.txt";
+		String test = "AzA9val2as\r\n" + 
+				"ba6b32SoWR\r\n" + 
+				"56yylg1SMY\r\n" + 
+				"2ns4WUYYl5\r\n" + 
+				"cyypIqFP3Z\r\n" + 
+				"mB8qeZdefH\r\n" + 
+				"kC4ITOgOqG\r\n" + 
+				"KwhQutszWT\r\n" + 
+				"mZ5WHMUeWP\r\n" + 
+				"wZaRb7MZS5";
+		try {
+			String src = Main.readFileToString(path);
+			assertEquals(src, test);
+		} catch (IOException e) {
+			System.err.println("Bad Test Setup");
+			e.printStackTrace();
+		}
 	}
 	
 }
